9535a0b79e6199b94498376e10eb693ba505d905
Aurélie Mlynarz <aureliemlynarz@macbook-pro-de-aurelie.home>
2020-11-14 17:11:04 +0100

    ajout template

--- gatsby-portfolio/gatsby-node.js
+++ gatsby-portfolio/gatsby-node.js
@@ -1,15 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions
+    const { createNodeField } = actions;
     if (node.internal.type === 'MarkdownRemark') {
         const slug = createFilePath ({ node, getNode, basePath: 'content'});
         createNodeField({
@@ -18,4 +11,28 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
             value: slug,
         })
     }
+}
+
+exports.createPages = async function ({ actions, graphql }) {
+    const { data } = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+    data.allMarkdownRemark.edges.forEach(edge => {
+        const { slug } = edge.node.fields;
+        actions.createPage({
+            path: slug,
+            component: require.resolve(`./src/template/posts.js`),
+            context: {slug: slug},
+        })
+    })
 }
