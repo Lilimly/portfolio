4936f2d2a1dbbac78063781741fa45959c0cdb8d
Lilimly <aureliemlynarz@gmail.com>
2021-02-03 14:58:42 +0100

    Font comfortaa local

--- gatsby-portfolio/gatsby-ssr.js
+++ gatsby-portfolio/gatsby-ssr.js
@@ -1,7 +1,1 @@
 import "./src/css/index.css"
-import RootLayout from "./src/components/RootLayout.jsx"
-import React from "react"
-
-export const wrapRootElement = ({ element }) => (
-  <RootLayout>{element}</RootLayout>
-)
